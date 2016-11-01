7502effb67fd6a7b80fcdcc2d281569b106bd619
Dan Abramov <dan.abramov@gmail.com>
2016-11-01 19:13:52 +0000

    Rewrite comments to put less emphasis on deferred/animation
    We intend to change this system later.

--- src/renderers/shared/fiber/__tests__/ReactIncrementalScheduling-test.js
+++ src/renderers/shared/fiber/__tests__/ReactIncrementalScheduling-test.js
@@ -70,7 +70,7 @@ describe('ReactIncrementalScheduling', () => {
     expect(ReactNoop.getChildren('a')).toEqual([span('a:6')]);
   });
 
-  it('works on deferred roots in the order they were scheduled', () => {
+  it('handles lo-pri roots in the order they were scheduled', () => {
     ReactNoop.renderToRootWithID(<span prop="a:1" />, 'a');
     ReactNoop.renderToRootWithID(<span prop="b:1" />, 'b');
     ReactNoop.renderToRootWithID(<span prop="c:1" />, 'c');
@@ -79,7 +79,7 @@ describe('ReactIncrementalScheduling', () => {
     expect(ReactNoop.getChildren('b')).toEqual([span('b:1')]);
     expect(ReactNoop.getChildren('c')).toEqual([span('c:1')]);
 
-    // Schedule deferred work in the reverse order
+    // Schedule lo-pri work in the reverse order
     ReactNoop.renderToRootWithID(<span prop="c:2" />, 'c');
     ReactNoop.renderToRootWithID(<span prop="b:2" />, 'b');
     // Ensure it starts in the order it was scheduled
@@ -100,7 +100,7 @@ describe('ReactIncrementalScheduling', () => {
     expect(ReactNoop.getChildren('c')).toEqual([span('c:2')]);
   });
 
-  it('works on roots with animation priority before deferred work', () => {
+  it('handles hi-pri roots before flushing lo-pri roots', () => {
     ReactNoop.renderToRootWithID(<span prop="a:1" />, 'a');
     ReactNoop.renderToRootWithID(<span prop="b:1" />, 'b');
     ReactNoop.renderToRootWithID(<span prop="c:1" />, 'c');
@@ -109,35 +109,41 @@ describe('ReactIncrementalScheduling', () => {
     expect(ReactNoop.getChildren('b')).toEqual([span('b:1')]);
     expect(ReactNoop.getChildren('c')).toEqual([span('c:1')]);
 
-    // Schedule both deferred and animation work
+    // Schedule lo-pri work
     ReactNoop.renderToRootWithID(<span prop="a:2" />, 'a');
+    // Schedule hi-pri work
     ReactNoop.performAnimationWork(() => {
       ReactNoop.renderToRootWithID(<span prop="b:2" />, 'b');
       ReactNoop.renderToRootWithID(<span prop="c:2" />, 'c');
     });
-    // We're flushing deferred work
-    // Still, roots with animation work are handled first
+
+    // We're flushing lo-pri work
+    // Still, roots with hi-pri work are handled first
     ReactNoop.flushDeferredPri(15);
     expect(ReactNoop.getChildren('a')).toEqual([span('a:1')]);
     expect(ReactNoop.getChildren('b')).toEqual([span('b:2')]);
     expect(ReactNoop.getChildren('c')).toEqual([span('c:1')]);
+    // And then lo-pri roots are handled
     ReactNoop.flushDeferredPri(15);
     expect(ReactNoop.getChildren('a')).toEqual([span('a:1')]);
     expect(ReactNoop.getChildren('b')).toEqual([span('b:2')]);
     expect(ReactNoop.getChildren('c')).toEqual([span('c:2')]);
 
-    // More deferred and animation work just got scheduled!
+    // We still haven't processed some lo-pri work by now.
+    // Schedule more lo-pri work
     ReactNoop.renderToRootWithID(<span prop="c:3" />, 'c');
+    // Schedule more hi-pri work
     ReactNoop.performAnimationWork(() => {
       ReactNoop.renderToRootWithID(<span prop="b:3" />, 'b');
     });
-    // Animation is still handled first
+
+    // We're flushing lo-pri work
+    // Still, roots with hi-pri work are handled first
     ReactNoop.flushDeferredPri(15);
     expect(ReactNoop.getChildren('a')).toEqual([span('a:1')]);
     expect(ReactNoop.getChildren('b')).toEqual([span('b:3')]);
     expect(ReactNoop.getChildren('c')).toEqual([span('c:2')]);
-
-    // Finally we handle deferred root in the order it was scheduled
+    // Finally, lo-pri roots are handled in the order they were scheduled
     ReactNoop.flushDeferredPri(15);
     expect(ReactNoop.getChildren('a')).toEqual([span('a:2')]);
     expect(ReactNoop.getChildren('b')).toEqual([span('b:3')]);
